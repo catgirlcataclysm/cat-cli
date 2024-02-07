2fe7ab13a5a4405b54a2843967c7cd8196781811
Kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2024-02-07 13:30:17 -0500

    makes cats

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use reqwest::get;
-use reqwest::Error;
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
 use winit::dpi::LogicalSize;
+use exitfailure::ExitFailure;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -14,31 +14,33 @@ struct ImgData {
 }
 
 impl ImgData {
-    async fn get_url() -> Result<String, Error> {
-        let data = get("https://api.thecatapi.com/v1/images/search").await?.json::<ImgData>().await?;
-        let url = data.url;
-        return Ok(url)
-        // TODO: FIX THIS SHIT
-    }
+  async fn get() -> Result<Option<ImgData>, ExitFailure> {
+        let img = get("https://api.thecatapi.com/v1/images/search").await?.json::<(ImgData,)>().await?;
+        //  remove will panic if less than 2 items in imgdata
+        Ok(Some(img.0))
+    }  
 }
 
-fn make_window(transparent: bool, blur: bool) {
-
+fn make_window() {
     let event_loop = EventLoop::new().expect("Failed to create EventLoop");
     let window_create = WindowBuilder::new();
     window_create
         .with_min_inner_size(LogicalSize::new(200.0, 200.0))
-        .with_transparent(transparent)
-        .with_blur(blur)
         .with_title("C.A.T. : Cat Aquiring Tool")
         .build(&event_loop)
         .expect("Failed to create window");
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
     //  Creates the window using variables for transparency and blur
     // TODO: Allow those options to be set by users in app
-    make_window(true, true);
+    make_window();
     // TODO: IMPLEMENT GUI TO FETCH NEW IMAGE
-    // Queries the API @ https://api.thecatapi.com/v1/images/search to get a random cat image
+    // Queries the API @ https://api.thecatapi.com/v1/images/search to get ImgData for a random cat image
+    let img = ImgData::get().await?.unwrap();
+    println!("{} {} {} {}", img.url, img.width, img.height, img.id);
+    Ok(())
+
+
 }
